fb002aaa5c7e8b2ea75fe439373fc9fae3f79597
facbenitez <facbenitez@alumno.unlam.edu.ar>
2023-05-02 21:54:52 -0300

    se crearon los constructores de cargas y montacargas con sus getter y setter

--- src/ar/edu/unlam/pb2/tp3Montacargas/Cargas.java
+++ src/ar/edu/unlam/pb2/tp3Montacargas/Cargas.java
@@ -4,5 +4,37 @@ public class Cargas {
 	private Integer id;
 	private Double peso;
 	private String propietario;
+	
+	public Cargas(Integer id, Double peso, String propietario) {
+		this.id = id;
+		this.peso = peso;
+		this.propietario = propietario;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+
+	public String getPropietario() {
+		return propietario;
+	}
+
+	public void setPropietario(String propietario) {
+		this.propietario = propietario;
+	}
+	
+	
 
 }
--- src/ar/edu/unlam/pb2/tp3Montacargas/Montacargas.java
+++ src/ar/edu/unlam/pb2/tp3Montacargas/Montacargas.java
@@ -1,10 +1,27 @@
 package ar.edu.unlam.pb2.tp3Montacargas;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Montacargas {
 	private Double pesoMaximo;
 	private List<Cargas>listaDeCargas;
+	
+	public Montacargas(Double pesoMaximo) {
+		this.pesoMaximo = pesoMaximo;
+		this.listaDeCargas = new ArrayList<Cargas>();
+		
+	}
+
+	public Double getPesoMaximo() {
+		return pesoMaximo;
+	}
+
+	public void setPesoMaximo(Double pesoMaximo) {
+		this.pesoMaximo = pesoMaximo;
+	}
+
+
 
 
 }
